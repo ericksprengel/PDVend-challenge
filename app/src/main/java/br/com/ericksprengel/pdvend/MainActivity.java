68b5ff1a59068a352f7f5cff8826eefcc1391092
ERICK SPRENGEL/LGESP RErick.sprengelD MC SW(erick.sprengel@lge.com) <erick.sprengel@lge.com>
2016-11-29 23:19:01 -0200

    final

--- app/src/main/java/br/com/ericksprengel/pdvend/MainActivity.java
+++ app/src/main/java/br/com/ericksprengel/pdvend/MainActivity.java
@@ -1,30 +1,50 @@
 package br.com.ericksprengel.pdvend;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String ACTION_PAYMENT = "br.com.ericksprengel.intent.action.PAYMENT";
+    public static final String EXTRA_VALUE = "value";
+
+    private static final String RESULT_ACTION_CHANGE = "br.com.ericksprengel.intent.result.PAYMENT";
+    private static final String RESULT_EXTRA_CHANGE = "change";
+
+    private View mContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContentView = findViewById(R.id.main_activity_root);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    }
+
+    public void showMessage(String msg, String action, View.OnClickListener listener) {
+        Snackbar.make(mContentView, msg, Snackbar.LENGTH_LONG).setAction(action, listener).show();
+    }
+
+    public void showMessage(String msg) {
+        Snackbar.make(mContentView, msg, Snackbar.LENGTH_LONG).show();
+    }
+
+    public void finishPayment(double change) {
+        Log.i(BuildConfig.LOG_TAG, "Payment finishing... Change: " + NumberFormat.getCurrencyInstance().format(change));
+        Intent result = new Intent(RESULT_ACTION_CHANGE);
+        result.putExtra(RESULT_EXTRA_CHANGE, change);
+        setResult(AppCompatActivity.RESULT_OK, result);
+        finish();
     }
 }
--- app/src/main/java/br/com/ericksprengel/pdvend/MainActivityFragment.java
+++ app/src/main/java/br/com/ericksprengel/pdvend/MainActivityFragment.java
@@ -1,12 +1,18 @@
 package br.com.ericksprengel.pdvend;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
@@ -24,16 +30,24 @@ import java.util.List;
 public class MainActivityFragment extends ListFragment implements View.OnClickListener {
 
     final NumberFormat mCurrencyFormat = NumberFormat.getCurrencyInstance();
+    final NumberFormat mNumberFormat = NumberFormat.getNumberInstance();
 
     private TextView mPaymentTotalValue;
     private EditText mPaymentCurrentValue;
     private TextView mPaymentPendingValue;
+    private Button mMoneyButton;
+    private Button mCreditButton;
+    private Button mSendButton;
+
 
     private double mTotalValue;
     private PaymentsListAdapter mAdapter;
+    private TextWatcher mCurrencyWatcher = new CurrencyWatcherFormatter();
 
     public MainActivityFragment() {
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,26 +59,62 @@ public class MainActivityFragment extends ListFragment implements View.OnClickLi
         mPaymentCurrentValue = (EditText) root.findViewById(R.id.payment_fragment_current_value);
         mPaymentPendingValue = (TextView) root.findViewById(R.id.payment_fragment_pending_value);
 
-        root.findViewById(R.id.payment_fragment_money_button).setOnClickListener(this);
-        root.findViewById(R.id.payment_fragment_credit_button).setOnClickListener(this);
+        mMoneyButton = (Button) root.findViewById(R.id.payment_fragment_money_button);
+        mCreditButton = (Button) root.findViewById(R.id.payment_fragment_credit_button);
+        mSendButton = (Button) root.findViewById(R.id.payment_fragment_send_button);
 
-        mTotalValue = 500.00;
+        mMoneyButton.setOnClickListener(this);
+        mCreditButton.setOnClickListener(this);
+        mSendButton.setOnClickListener(this);
+        root.findViewById(R.id.payment_fragment_cancel_button).setOnClickListener(this);
+
+        mPaymentCurrentValue.addTextChangedListener(mCurrencyWatcher);
+        mNumberFormat.setMinimumFractionDigits(2);
 
         List<Payment> payments = new ArrayList<>();
-        payments.add(new Payment(Payment.PaymentType.MONEY, 123.45));
-        payments.add(new Payment(Payment.PaymentType.CREDIT, 20.35));
+        // payments.add(new Payment(Payment.PaymentType.MONEY, 123.45));
+        // payments.add(new Payment(Payment.PaymentType.CREDIT, 20.35));
         mAdapter = new PaymentsListAdapter(getLayoutInflater(savedInstanceState), payments);
         setListAdapter(mAdapter);
 
-        updateView();
+        mPaymentCurrentValue.setText(mCurrencyFormat.format(mTotalValue));
+
         return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Intent intent = getActivity().getIntent();
+
+        if(intent.getAction() != MainActivity.ACTION_PAYMENT) {
+            mTotalValue = 1100.00;
+            getMainActivity().showMessage("I'm starting a payment test with fixed value: " + NumberFormat.getCurrencyInstance().format(mTotalValue));
+        } else if(intent.hasExtra(MainActivity.EXTRA_VALUE)) {
+            mTotalValue = intent.getDoubleExtra(MainActivity.EXTRA_VALUE, 0.00);
+        } else {
+            Toast.makeText(getContext(), "Error: invalid intet", Toast.LENGTH_LONG).show();
+            getMainActivity().finish();
+        }
+        updateView();
     }
 
     private void updateView() {
         double totalValueAdded = mAdapter.getTotalValue();
 
         mPaymentTotalValue.setText(mCurrencyFormat.format(mTotalValue));
-        mPaymentPendingValue.setText(mCurrencyFormat.format(mTotalValue - totalValueAdded));
+        mPaymentPendingValue.setText(mNumberFormat.format(mTotalValue - totalValueAdded));
+
+        double totalValueByCreditAdded = mAdapter.getTotalValueByCredit();
+        if(totalValueAdded >= mTotalValue) {
+            mCreditButton.setEnabled(false);
+            mMoneyButton.setEnabled(false);
+            mSendButton.setEnabled(true);
+        } else {
+            mCreditButton.setEnabled(!(totalValueByCreditAdded + getCurrentValue() > mTotalValue));
+            mMoneyButton.setEnabled(true);
+            mSendButton.setEnabled(totalValueAdded >= mTotalValue);
+        }
     }
 
     @Override
@@ -78,7 +128,17 @@ public class MainActivityFragment extends ListFragment implements View.OnClickLi
                 mAdapter.addPayment(new Payment(Payment.PaymentType.CREDIT, getCurrentValue()));
                 updateView();
                 break;
+            case R.id.payment_fragment_send_button:
+                getMainActivity().finishPayment(getChange());
+                break;
+            case R.id.payment_fragment_cancel_button:
+                getActivity().finish();
+                break;
         }
+    }
+
+    private double getChange() {
+        return mAdapter.getTotalValue() - mTotalValue;
     }
 
     public double getCurrentValue() {
@@ -89,6 +149,10 @@ public class MainActivityFragment extends ListFragment implements View.OnClickLi
             Toast.makeText(getContext(), "Desculpe-me, mas esta operação não pode ser concluída.", Toast.LENGTH_LONG).show();
             return 0.0;
         }
+    }
+
+    public MainActivity getMainActivity() {
+        return (MainActivity) getActivity();
     }
 
     private class PaymentsListAdapter extends BaseAdapter {
@@ -145,6 +209,17 @@ public class MainActivityFragment extends ListFragment implements View.OnClickLi
             return res;
         }
 
+        public double getTotalValueByCredit() {
+            double res = 0.0;
+            for(Payment p : mPayments) {
+                if(p.type.equals(Payment.PaymentType.CREDIT)) {
+                    res += p.value;
+                }
+            }
+            // mPayments.stream().mapToDouble(p -> p.value).reduce(0.0, Double::sum);
+            return res;
+        }
+
         public void addPayment(Payment payment) {
             mPayments.add(payment);
             notifyDataSetChanged();
@@ -156,5 +231,35 @@ public class MainActivityFragment extends ListFragment implements View.OnClickLi
             TextView value;
         }
 
+    }
+
+    private class CurrencyWatcherFormatter implements TextWatcher {
+        private String current = "";
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!s.toString().equals(current)) {
+                mPaymentCurrentValue.removeTextChangedListener(this);
+
+                String cleanString = s.toString().replaceAll("[\\D]", "");
+
+                double value = Double.parseDouble(cleanString) / 100;
+                String formatted = mCurrencyFormat.format(value);
+
+                current = formatted;
+                mPaymentCurrentValue.setText(formatted);
+                mPaymentCurrentValue.setSelection(formatted.length());
+
+                mPaymentCurrentValue.addTextChangedListener(this);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            updateView();
+        }
     }
 }
